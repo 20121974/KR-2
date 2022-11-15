804edd4e5217aabf340790f443f9f898f54320d8
20121974 <nfi46102@mail.ru>
2022-11-15 22:13:31 +0300

    5 commit

--- src/Programs.java
+++ src/Programs.java
@@ -1,25 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Programs {
-
-    private static List<Integer> integers = new ArrayList<>(List.of(1, 2, 3));
-
+    private static List<Integer> integers = new ArrayList<>();
     public static void start() {
         for (int i = 0; i < integers.size(); i++) {
-            System.out.println(i);
+            System.out.println((i));
         }
     }
-    public static Object start1() {
+
+    public static void start2() {
         int i = 0;
         while (i < integers.size()) {
-            System.out.println();
+            System.out.println((i));
+            i++;
         }
-        return start1();
     }
-    public static void start2() {
+
+    public static void start3() {
         for (Integer integer:integers) {
-            System.out.println(integers);
+            System.out.println((integer));
         }
     }
 }
